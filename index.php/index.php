cd68acf1cc18d1b97d6aa5ff620f39e35fc9458c
unknown <s192318>
2020-12-02 15:37:13 +0900

    変更

---
+++ index.php/index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="ja">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, user-scalable=yes, maximum-scale=1.0, minimum-scale=1.0">
+<meta name="description" content="Cトラベル | あなたの旅行をサポートします！">
+<meta name="keywords" content="">
+<title>Cトラベル | あなたの旅行をサポートします！</title>
+<link rel="stylesheet" href="style.css" type="text/css" media="screen">
+</head>
+<body>
+    <header>
+  <div id="nav-drawer">
+      <input id="nav-input" type="checkbox" class="nav-unshown">
+      <label id="nav-open" for="nav-input"><span></span></label>
+      <label class="nav-unshown" id="nav-close" for="nav-input"></label>
+      <div id="nav-content">
+        <ul id="menu">
+            <li><a href="#">ログイン</a></li>
+            <li><a href="#">ホーム</a></li>
+            <li><a href="#">プラン作成</a></li>
+            <li><a href="#">ユーザ設定</a></li>
+        </ul></div>
+  </div>
+</header>
+<h1>Cトラベル</h1>
+<div id="wrapper">
+    <div class="kensaku">
+    <input type="search" name="search" placeholder="キーワードを入力">
+  <input type="submit" name="submit" value="検索">
+    </div>
+<div class="img">    
+    <div>
+        <a href="aaa.png"><img src="aaa.png"></a>
+      <img src="aaa.png">
+      <img src="aaa.png">
+    </div>
+    <div>
+      <img src="aaa.png">
+      <img src="aaa.png">
+      <img src="aaa.png">
+    </div>
+    <div>
+      <img src="aaa.png">
+      <img src="aaa.png">
+      <img src="aaa.png">
+    </div>
+
+    </div>
+</div>
+
+
+</body>
+</html>
