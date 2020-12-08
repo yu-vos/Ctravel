ed10e37d83555044a59ba39f398f681ad1ec7d1e
unknown <s192207>
2020-12-08 13:12:42 +0900

    12/08

--- Login/js/firebaseauth.js
+++ Login/js/firebaseauth.js
@@ -1,6 +1,7 @@
-(function () {
-
-  var firebaseConfig = {
+(function(){
+    // Your web app's Firebase configuration
+    // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+    var firebaseConfig = {
     apiKey: "AIzaSyC9KcYrMwFoR7VASRqKzEVz-Gb3y3H4hoc",
     authDomain: "ctravel-ss2h.firebaseapp.com",
     databaseURL: "https://ctravel-ss2h.firebaseio.com",
@@ -10,51 +11,43 @@
     appId: "1:586418241988:web:1fec8ae8a0347ed6c12491",
     measurementId: "G-Z8CDM91H2R"
   };
-  // Initialize Firebase
+   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
   firebase.analytics();
+  
+    const txtEmail = document.getElementById('txtEmail');
+    const txtPassword = document.getElementById('txtPassword');
+    const btnLogin = document.getElementById('btnLogin');
+    const btnSignUp = document.getElementById('btnSignUp');
+    const btnLogout = document.getElementById('btnLogout');
 
-  const txtEmail = document.getElementById('txtEmail');
-  const txtPassword = document.getElementById('txtPassword');
-  const btnLogin = document.getElementById('btnLogin');
-  const btnSignUp = document.getElementById('btnSignUp');
-  const btnLogout = document.getElementById('btnLogout');
-
-  btnLogin.addEventListener('click', e => {
-
-    const email = txtEmail.value;
-    const pass = txtPassword.value;
-    const autu = firebase.auth();
-
-    const promise = autu.signInWithEmailPassword(email, pass);
-    promise.catch(e => console.log(e.meassge));
-
-  });
-
-  btnSignUp.addEventListener('click', e => {
-
-    const email = txtEmail.value;
-    const pass = txtPassword.value;
-    const autu = firebase.auth();
-
-    const promise = autu.signInWithEmailPassword(email, pass);
-    promise
-      .catch(e => console.log(e.meassge));
-
-  });
-
-  btnLogout.addEventListener('click', e => {
-    firebase.auth().signOut();
-  });
-
-  firebase.auth().onAuthStateChanged(firebaseUser => {
-    if (firebaseUser) {
-      console.log(firebaseUser);
-      btnLogout.classList.remove('hide');
-    } else {
-      console.log("not logged in")
-      btnLogout.classList.add('hide');
-    };
-  });
-
+    btnLogin.addEventListener('click', e => {
+        const email = txtEmail.value;
+        const pass = txtPassword.value;
+        const auth = firebase.auth();
+        const promise = auth.signInWithEmailAndPassword(email,pass);
+        promise.catch(e => console.log(e.message));
+    });
+    
+    btnSignUp.addEventListener('click', e => {
+        const email = txtEmail.value;
+        const pass = txtPassword.value;
+        const auth = firebase.auth();
+        const promise = auth.createUserWithEmailAndPassword(email,pass);
+        promise.catch(e => console.log(e.message));
+    });
+   
+    btnLogout.addEventListener('click', e=>{
+       firebase.auth().signOut(); 
+    });
+    
+    firebase.auth().onAuthStateChanged(firbaseUser => {
+        if(firbaseUser){
+            console.log(firbaseUser);
+            btnLogout.classList.remove('hide');
+        }else{
+            console.log('not logged in');
+            btnLogout.classList.add('hide');
+        };
+    });
 }());
--- Login/js/home.js
+++ Login/js/home.js
@@ -1,26 +1,22 @@
-// ポップアップの表示挙動
-window.addEventListener('DOMContentLoaded',function () {
-    $('#openModal').click(function(){
-        $('#modalArea').fadeIn();
-    });
-    $('#closeModal , #modalBg').click(function(){
-    　  $('#modalArea').fadeOut();
-    });
-});
+  const btnSignUp = document.getElementById('btnSignUp');
+  const txtEmail = document.getElementById('txtEmail');
+  const txtPassword = document.getElementById('txtPassword');
 
-firebase.auth().signInWithPopup(provider).then(function(result) {
-  // This gives you a Google Access Token. You can use it to access the Google API.
-　var token = result.credential.accessToken;
-  // The signed-in user info.
-　var user = result.user;
-  // ...
-}).catch(function(error) {
-  // Handle Errors here.
-　var errorCode = error.code;
-　var errorMessage = error.message;
-  // The email of the user's account used.
-　var email = error.email;
-  // The firebase.auth.AuthCredential type that was used.
-　var credential = error.credential;
-  // ...
-});
+  firebase.auth().createUserWithEmailAndPassword(email, password)
+    .then((user) => {
+      // Signed in
+      // ...
+      const email = txtEmail.value;
+      const pass = txtPassword.value;
+      const autu = firebase.auth();
+
+      const promise = autu.signInWithEmailPassword(email, pass);
+      promise
+        .catch(e => console.log(e.meassge));
+
+    })
+    .catch((error) => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // ..
+    });
