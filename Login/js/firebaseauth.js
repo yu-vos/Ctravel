ca80274b7c3fdb67930e64a0a07c17b6a07f8ed8
unknown <s192207>
2020-12-09 15:45:48 +0900

    アカウント登録

--- Login/js/firebaseauth.js
+++ Login/js/firebaseauth.js
@@ -1,53 +1,48 @@
-(function(){
-    // Your web app's Firebase configuration
-    // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-    var firebaseConfig = {
-    apiKey: "AIzaSyC9KcYrMwFoR7VASRqKzEVz-Gb3y3H4hoc",
-    authDomain: "ctravel-ss2h.firebaseapp.com",
-    databaseURL: "https://ctravel-ss2h.firebaseio.com",
-    projectId: "ctravel-ss2h",
-    storageBucket: "ctravel-ss2h.appspot.com",
-    messagingSenderId: "586418241988",
-    appId: "1:586418241988:web:1fec8ae8a0347ed6c12491",
-    measurementId: "G-Z8CDM91H2R"
-  };
-   // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  firebase.analytics();
-  
-    const txtEmail = document.getElementById('txtEmail');
-    const txtPassword = document.getElementById('txtPassword');
-    const btnLogin = document.getElementById('btnLogin');
-    const btnSignUp = document.getElementById('btnSignUp');
-    const btnLogout = document.getElementById('btnLogout');
+// signup
+const signupForm = document.querySelector('#signup-form');
 
-    btnLogin.addEventListener('click', e => {
-        const email = txtEmail.value;
-        const pass = txtPassword.value;
-        const auth = firebase.auth();
-        const promise = auth.signInWithEmailAndPassword(email,pass);
-        promise.catch(e => console.log(e.message));
+if (signupForm) {
+    signupForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        // get user info
+        const email = signupForm['signup-email'].value;
+        const password = signupForm['signup-password'].value;
+
+        // sign up the user
+        auth.createUserWithEmailAndPassword(email, password).then(cred => {
+            // close the signup modal & reset form
+            const modal = document.querySelector('#modal-signup');
+            M.Modal.getInstance(modal).close();
+            signupForm.reset();
+        });
     });
-    
-    btnSignUp.addEventListener('click', e => {
-        const email = txtEmail.value;
-        const pass = txtPassword.value;
-        const auth = firebase.auth();
-        const promise = auth.createUserWithEmailAndPassword(email,pass);
-        promise.catch(e => console.log(e.message));
+}
+// logout
+const logout = document.querySelector('#logout');
+logout.addEventListener('click', (e) => {
+    e.preventDefault();
+    auth.signOut().then(() => {
+        console.log('user signed out');
+    })
+});
+
+// login
+const loginForm = document.querySelector('#login-form');
+loginForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    // get user info
+    const email = loginForm['login-email'].value;
+    const password = loginForm['login-password'].value;
+
+    // log the user in
+    auth.signInWithEmailAndPassword(email, password).then((cred) => {
+        console.log(cred.user);
+        // close the signup modal & reset form
+        const modal = document.querySelector('#modal-login');
+        M.Modal.getInstance(modal).close();
+        loginForm.reset();
     });
-   
-    btnLogout.addEventListener('click', e=>{
-       firebase.auth().signOut(); 
-    });
-    
-    firebase.auth().onAuthStateChanged(firbaseUser => {
-        if(firbaseUser){
-            console.log(firbaseUser);
-            btnLogout.classList.remove('hide');
-        }else{
-            console.log('not logged in');
-            btnLogout.classList.add('hide');
-        };
-    });
-}());
+
+});
